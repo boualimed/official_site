30881aa913792ebdb19593081985f390ca5371bc
boualimed <bmbouali@gmail.com>
2018-08-13 21:46:15 +0800

    edited

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,12 @@
 module.exports = {
   
   pathPrefix: `/official_site`,
-  siteMetadata: {
-    title: `gatsby-example-using-excel`,
-    description: `Blazing-fast React.js static site generator`,
-                },
+   
 
    
 
   plugins: [
 
-     
-    `gatsby-transformer-excel`,
-    `gatsby-transformer-remark`,
-     
-    {
-      resolve: `gatsby-source-filesystem`,
-      options:    {
-        path: `${__dirname}/src/data`,
-        name: `data`,
-                 },
-                   },
      
     {
       resolve: `gatsby-plugin-typography`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,82 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
-import indexStyles from "../styles/index.module.css"
+ 
 
-class IndexComponent extends React.Component {
-  render() {
-    const data1 = this.props.data.allLettersXlsxSheet1.edges
-    const data2 = this.props.data.allLettersXlsxSheet2.edges
-    return (
-      <Layout> 
-      <h1 className={indexStyles.index}>Hi! I'm building a this site as part of personal food computer that contributed by openAg initiative</h1>
+export default () => (
+  <Layout>
+    <h1>Hi! I'm building a Gatsby site as part of a tutorial!</h1>
     <p>
-      a Lots of course but definitely enjoy
-      building future sustainable food.
+      What do I like to do? Lots of course but definitely enjoy
+      building websites.
     </p>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th colSpan="2">Sheet1</th>
-            </tr>
-            <tr>
-              <th>Letter</th>
-              <th>ASCII Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data1.map((row, i) => (
-              <tr key={`${row.node.value} ${i}`}>
-                <td>{row.node.letter}</td>
-                <td>{row.node.value}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <table>
-          <thead>
-            <tr>
-              <th colSpan="2">Sheet2</th>
-            </tr>
-            <tr>
-              <th>Letter</th>
-              <th>ASCII Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data2.map((row, i) => (
-              <tr key={`${row.node.value} ${i}`}>
-                <td>{row.node.letter}</td>
-                <td>{row.node.value}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      </Layout>
-    )
-  }
-}
-
-export default IndexComponent
-
-export const IndexQuery = graphql`
-  query {
-    allLettersXlsxSheet1 {
-      edges {
-        node {
-          letter
-          value
-        }
-      }
-    }
-    allLettersXlsxSheet2 {
-      edges {
-        node {
-          letter
-          value
-        }
-      }
-    }
-  }
-`
+  </Layout>
+)
