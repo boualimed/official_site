e06bfd5bca4eaa33fa0a23903df0709a0eceb1ac
boualimed <bmbouali@gmail.com>
2018-08-13 20:48:35 +0800

    gatsby_config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,45 +4,30 @@ module.exports = {
   siteMetadata: {
     title: `gatsby-example-using-excel`,
     description: `Blazing-fast React.js static site generator`,
-  },
+                },
 
    
 
   plugins: [
 
-    /*****************/
+     
     `gatsby-transformer-excel`,
     `gatsby-transformer-remark`,
-    
-<<<<<<< HEAD
      
-=======
-    
->>>>>>> c253c4f3d9cf5bbeb29caf815e7fefeb54165b4d
-
-    
-
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
+      options:    {
         path: `${__dirname}/src/data`,
         name: `data`,
-      },
-    },
-    /*
-     * Gatsby's data processing layer begins with “source” plugins. Here we
-     * setup the site to pull data from the "documents" collection in a local
-     * MongoDB instance
-     */
-    
-    /*****************/
+                 },
+                   },
+     
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
                },
     },
-    /*****************/
      
   ],
   
